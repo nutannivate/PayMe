ac4587b71f0433cef36d390cea179826ac03906a
Sachin <psachin@forest.lan>
2018-05-25 00:02:48 +0530

    Initial commit

---
+++ app/src/androidTest/java/me/pay/nutan/paybyme/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package me.pay.nutan.paybyme
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("me.pay.nutan.paybyme", appContext.packageName)
+    }
+}
